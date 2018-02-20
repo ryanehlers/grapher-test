132048a476ac106a96430a53697ade16599f3f10
Ryan Ehlers <ryan.ehlers@oup.com>
2018-02-20 14:47:43 +0000

    Add headings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div>
+        <p>Standard:</p>
         <Grapher graphId="graph1" formula="2*x" zoom="false" showCoords="false"/>
+        <p>Drag/zoom-able:</p>
         <Grapher graphId="graph2" formula="x^2" zoom="true" showCoords="true"/>
       </div>
     );
